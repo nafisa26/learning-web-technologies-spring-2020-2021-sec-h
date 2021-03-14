3f1991fe817304d2b6bdc388115c50731a27e218
nafisa26 <nafisaanzum26@gmail.com>
2021-03-14 12:31:09 +0600

    1st commit at 12:30 pm

---
+++ LAB_EXAM_set-A/Registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<form method="POST" action="#">
+		<fieldset style="width:315px">
+			<legend><strong>REGISTRATION</strong></legend>
+			 		ID<br>
+			 		<input type="text" name="id" value=""><br></td><br>
+			 		Password<br>
+			 		<input type="password" name="pass" value=""><br>
+			 	Confirm Password<br>
+			 	<input type="password" name="cpass" value=""><br>
+				Name<br>
+				<input type="text" name="name" value=""><br>
+		
+			 		<td colspan="2"><input type="submit" name="submit" value="Submit">
+			 	</tr>
+			 </table>
+		</fieldset>
+	</form>
+
+</body>
+</html>
